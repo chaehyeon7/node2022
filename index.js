0e3bf88c0fe28bfd09b0aeefca491a893322d971
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-03-25 15:06:55 +0900

    web2 간단한 동적 서버페이지

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const http = require('http')
 const fs = require("fs");
+const url = require('url');
 
 const app = http.createServer(function(req,res){
-    let url = req.url;
+    let _url = req.url;
+    let queryData = url.parse(_url, true).query;
+    res.end(queryData.id);
 
-    if(url === '/')
-        url = '/index.html';
-    if(url === '/facicon.ico')
-        return res.writeHead(404);
-    res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + url));
+    // if(url === '/')
+    //     url = '/index.html';
+    // if(url === '/facicon.ico')
+    //     return res.writeHead(404);
+    // res.writeHead(200);
+    // res.end(fs.readFileSync(__dirname + url));
 });
 
 app.listen(3333);
