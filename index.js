2d69317522a2904f1f34b1f5d289136b240e3a06
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-05-30 15:10:45 +0900

    web3 9-1 미들웨어의 사용

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const template = require('./lib/template.js');
 const fs = require('fs');
+const qs = require("querystring");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', function (req, res) {
     fs.readdir('./data', function(err, filelist){
         const title = 'Welcome';
@@ -20,12 +25,12 @@ app.get('/page/:pageId', function (req, res) {
     fs.readdir('./data', function(err, filelist){
         const list = template.List(filelist);
         const id = req.params.pageId;
-        fs.readFile(`./data/${id}`, 'utf8', function (err, description){
+        fs.readFile(`./data/${id}`, 'utf8', function (error, description){
             const title = id;
             const html = template.HTML(title, list, description
                 , `<a href="/create">create</a>
-                <a href="/update?id=${title}">update</a> 
-                <form action="delete_process" method="post">
+                <a href="/update/${title}">update</a> 
+                <form action="/delete_process" method="post">
                     <input type="hidden" name="id" value="${title}">
                     <input type="submit" value="delete">
                 </form>`);
@@ -33,5 +38,84 @@ app.get('/page/:pageId', function (req, res) {
         })
     })
 })
+
+
+app.get('/create', function (req, res){
+    fs.readdir('./data', function (err, filelist) {
+        const title = 'WEB - create';
+        const list = template.List(filelist);
+        const html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
+        res.send(html);
+    });
+});
+
+app.post('/create_process', function(req, res){
+
+    const reqBody = req.body;
+    const title = post.title;
+    const description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+        res.redirect(`/page/${title}`);
+    });
+
+});
+
+app.get('/update/:pageId', function(req, res){
+    fs.readdir('./data', function (err, filelist) {
+        const id = req.params.pageId;
+        fs.readFile(`data/${id}`, 'utf8', function (error, description) {
+            const title = id;
+            const list = template.List(filelist);
+            const html = template.HTML(title, list,
+                `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+                `<a href="/create">create</a> <a href="/update/${title}">update</a>`,
+            );
+            res.send(html);
+        });
+    });
+});
+
+app.post('/update_process', function (req, res) {
+
+    const reqBody = req.body;
+    const id = post.id;
+    const title = post.title;
+    const description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+            res.redirect(`/page/${title}`);
+        });
+    });
+});
+
+app.post('/delete_process', function(req, res){
+
+        const reqBody = req.body;
+        const id = post.id;
+        fs.unlink(`data/${id}`, function (error) {
+            res.redirect(`/`);
+        });
+});
 
 app.listen(4444);
